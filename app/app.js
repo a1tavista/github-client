18e0b1d24e7ec4217e1586fb478b178c0f3c2cf0
Andrey Rudenets <andrey.rudenets@gmail.com>
2016-05-07 10:18:19 +0300

    Подключение Materialize, верстка основного шаблона страницы, добавление зависимостей для приложения

--- app/app.js
+++ app/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+var app = angular.module('githubClient', [
+    'ngRoute',
+    'githubClient.repositorySelect',
+    'githubClient.repositorySelect.services',
+    'githubClient.repositoryView',
+    'githubClient.repositoryView.services'
+]).config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/search'});
 }]);
